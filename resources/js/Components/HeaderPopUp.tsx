7661b3946dde31d512639cc98856c169b5ae4721
John Elarcosa Davila <davilajohn810@gmail.com>
2023-11-20 02:58:25 +0800

    [Hotfix] Revamped booking notification

--- resources/js/Components/HeaderPopUp.tsx
+++ resources/js/Components/HeaderPopUp.tsx
@@ -2,6 +2,7 @@ import { Box, Button, CloseButton, Collapse, Slide, useDisclosure } from "@chakr
 import PrimaryButton from "./PrimaryButton"
 import { router, usePage } from "@inertiajs/react"
 import React from "react"
+import { motion } from "framer-motion"
 
 const HeaderPopUp = () => {
     const {latestTransaction} = usePage().props
@@ -14,11 +15,11 @@ const HeaderPopUp = () => {
   return (
     <>
         <Collapse in={isOpen} animateOpacity>
-            <Box className="flex items-center px-4 sm:px-6 lg:px-8l w-full bg-[#092327] text-white h-12 md:h-24">
-                <div className="max-w-6xl mx-auto w-full text-xl md:text-3xl font-black tracking-widest ease-in-out text-center relative ">
-                    <span className="hover:bg-[#0b5351] select-none rounded p-4" onClick={() => router.get(route('booking.customerViewBooking', latestTransaction as string))}>
+            <Box className="flex items-center px-4 sm:px-6 lg:px-8l w-full bg-[#092327] text-white h-12 md:h-24" >
+                <div className="max-w-6xl mx-auto w-full text-xl md:text-3xl font-black tracking-widest ease-in-out text-center relative">
+                    <motion.div className="select-none rounded p-4" whileHover={{scale: 1.1}} onClick={() => router.get(route('booking.customerViewBooking', latestTransaction as string))}>
                         View booked venue
-                    </span>
+                    </motion.div>
                 </div>
                 <CloseButton type='button' className="absolute right-5 top-3 z-10" onClick={onClose}/>
             </Box>
