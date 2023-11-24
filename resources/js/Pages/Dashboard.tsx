cba8c9563c3bcaff934e9dfedd6929c884560564
John Elarcosa Davila <davilajohn810@gmail.com>
2023-11-25 06:08:55 +0800

    [Hotfix] Added Logo

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import { ITransaction, PageProps, TransactionArr } from '@/types';
 import ReservationTable from '@/Components/Reservation/ReservationTable';
 import RefundTable from '@/Components/Reservation/RefundTable';
 import { MdEmail } from "react-icons/md";
+import { HiOutlineMailOpen } from "react-icons/hi";
 import { motion } from 'framer-motion';
 import PrimaryButton from '@/Components/PrimaryButton';
 import SecondaryButton from '@/Components/SecondaryButton';
@@ -24,19 +25,24 @@ export default function Dashboard({ auth, transactions, refundRequests, emailCou
             <div className="md:py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 sm:space-y-7">
                 <div className='flex flex-col md:grid md:grid-cols-3  items-start grid-flow-col md:gap-10'>
-                    <div className='bg-white overflow-hidden shadow-sm sm:rounded-lg p-5 w-full'>
-                        <h1 className='font-black text-xl'>TOTAL MESSAGES</h1>
-                        <h1 className='font-black text-5xl text-indigo-500'>{emailCountInquiries}</h1>
+                    <div className='bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 w-full'>
+                        <div className='font-black text-xl flex items-center space-x-2'>
+                            <h1 className=''>TOTAL INQUIRY</h1>
+                            <MdEmail/>
+                        </div>
+                        <div className=' font-black text-5xl text-indigo-500'>
+                            <h1>{emailCountInquiries}</h1>
+                        </div>
                     </div>
-                    <div className='bg-white overflow-hidden shadow-sm sm:rounded-lg p-5 col-span-2 w-full'>
+                    <div className='bg-[#212529] overflow-hidden shadow-sm sm:rounded-lg p-10 col-span-2 w-full'>
                         <div className='flex flex-col'>
                             <div className='flex flex-row justify-between items-start mb-5'>
-                                <h1 className='font-black text-xl'>LATEST INQUIRY</h1>
+                                <h1 className='font-black text-xl text-white'>LATEST INQUIRY</h1>
                                 <SecondaryButton>View more</SecondaryButton>
                             </div>
                             <div className='flex flex-col space-y-6'>
                                 {latestEmailInquiries.map((inquiry: any, index: number) => (
-                                    <motion.div key={index} whileHover={{scale:1.02}} className='flex flex-col border-t-[2px] py-1 px-3 border-indigo-700 shadow-md min-h-[80px] relative'>
+                                    <motion.div key={index} whileHover={{scale:1.03}} className='flex flex-col border-t-[2px] py-1 px-3 border-indigo-700 shadow-md min-h-[80px] relative bg-white'>
                                         <div className='flex flex-row items-center justify-between'>
                                             <span className='text-[#e56b6f]'>
                                                 {inquiry.full_name}
@@ -45,7 +51,7 @@ export default function Dashboard({ auth, transactions, refundRequests, emailCou
                                                 <span>
                                                     {inquiry.email}
                                                 </span>
-                                                <MdEmail/>
+                                                <HiOutlineMailOpen/>
                                             </div>
                                         </div>
                                         <div className='flex flex-col justify-between'>
