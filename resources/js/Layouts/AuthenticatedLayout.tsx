12b0202b7c3edecabc4cc5723ea5d20917bfa8f6
John Elarcosa Davila <davilajohn810@gmail.com>
2023-12-07 08:42:57 +0800

    [Hotfix] Navbar admin logo

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -102,7 +102,7 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <img src={`/icon.svg`} alt="idylic logo" className='block h-12 w-auto fill-curren' loading='lazy'/>
                                 </Link>
                             </div>
 
